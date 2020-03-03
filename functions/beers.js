8a34aa75957ce84b258bd59f402051e01c492c7d
Johan Runsten <johanrunsten86@gmail.com>
2020-03-03 12:08:49 +0100

    Test read db from function

--- functions/beers.js
+++ functions/beers.js
@@ -1,12 +1,25 @@
-exports.handler = (event, context, callback) => {
+import { faunadb, query as q } from 'faunadb';
+
+const client = new faunadb.Client({
+    secret: process.env.FAUNADB_SERVER_SECRET
+});
+
+exports.handler = async (event, context) => {
+    console.log('process.env', process.env);
     console.log('event', event);
     console.log('context', context);
 
-    return callback(null, {
-        statusCode: 200,
+    try {
+        const response = await client.query(q.Paginate(q.Match(q.Index('beers'))));
+    } catch (err) {
+        return {
+            stausCode: 500,
+            body: JSON.stringify(err)
+        };
+    }
 
-        body: JSON.stringify({
-            data: '⊂◉‿◉つ'
-        })
-    });
+    return {
+        statusCode: 200,
+        body: JSON.stringify(response)
+    };
 }
