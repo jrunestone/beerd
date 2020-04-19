fa47177e12014ff74b9af03cbe7d28ef33ec1cc3
Johan Runsten <johanrunsten86@gmail.com>
2020-04-19 22:14:37 +0200

    Reset style filter

--- src/components/StyleControlMenu/StyleControlMenu.vue
+++ src/components/StyleControlMenu/StyleControlMenu.vue
@@ -1,5 +1,6 @@
 <template>
     <ul>
+        <li><a href="#" @mousedown="changeStyleFilter(null)"><global-icon />ALL</a></li>
         <li v-for="(style, index) in styles" :key="index"><a href="#" @mousedown="changeStyleFilter(style)"><global-icon />{{ style.abbrName }}</a></li>
     </ul>
 </template>
