4fcfb57a4ae63914e6d9c7d550eb4dc04d30840b
Johan Runsten <johanrunsten86@gmail.com>
2020-07-29 22:00:43 +0200

    Hotfix SW

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const workbox = new WorkboxSW();
-
 workbox.routing.registerRoute('/.netlify/functions/beers', workbox.strategies.cacheFirst({
     cacheName: 'api-cache'
 }));
