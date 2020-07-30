bb29c0432847f426a90ca3c6b4a879ffa11ca9f1
Johan Runsten <johanrunsten86@gmail.com>
2020-07-30 21:00:05 +0200

    Trying to fix SW

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-workbox.routing.registerRoute('/.netlify/functions/beers', workbox.strategies.cacheFirst({
+workbox.routing.registerRoute('/.netlify/functions/beers', new workbox.strategies.CacheFirst({
     cacheName: 'api-cache'
 }));
