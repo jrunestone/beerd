3f2345209763a1c22b6776cb9b998bfbd302e29b
Johan Runsten <johanrunsten86@gmail.com>
2020-08-02 19:50:29 +0200

    Cache remote images with SW

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,3 +3,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 workbox.routing.registerRoute('/.netlify/functions/beers', new workbox.strategies.CacheFirst({
     cacheName: 'api-cache'
 }));
+
+workbox.routing.registerRoute(new RegExp('https://untappd\\.akamaized\\.net/.+\\.jpe?g'), new workbox.strategies.CacheFirst({
+    cacheName: 'image-cache'
+}));
